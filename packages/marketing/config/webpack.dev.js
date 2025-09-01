5941df2e3dde2980a8d2c9e5e91c462824d635e9
Gaurav Shrestha <gaurav.shrestha@kotak.com>
2025-09-01 15:35:43 +0530

    initial commit

---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,28 @@
+const {merge} = require('webpack-merge');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin =require('webpack/lib/container/ModuleFederationPlugin');
+const devConfig = {
+    mode: 'development',
+    devServer: {
+        port: 8081,
+        historyApiFallback: {
+            index: 'index.html'
+        },
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingApp': './src/bootstrap'
+            },
+            shared: ['react','react-dom']
+        }),
+        new HTMLWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, devConfig);
