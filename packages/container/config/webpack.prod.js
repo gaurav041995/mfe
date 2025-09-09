bbeb52c8a1e1b451282f8f76f4c98d737ca97a76
Gaurav Shrestha <gaurav.shrestha@kotak.com>
2025-09-09 14:25:36 +0530

    fix config

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,7 +1,7 @@
 const {merge} = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin =require('webpack/lib/container/ModuleFederationPlugin');
-const packageJson = require('../../package.json');
+const packageJson = require('../package.json');
 const domain= process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
     mode: 'production',
